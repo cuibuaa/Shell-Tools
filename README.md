1f7abeaf20edbe48ae086c505028bb5aad124321
Qian Cui <cuibuaa@gmail.com>
2015-01-25 13:06:23 +0800

    The inital version of the shell tools
    The function is like the following
    1) RARP function
    2) Scan the idle IP in one LAN range
    3) Wake on LAN any PC given a MAC address
    4) Remote connectiont with the PC by remote tools in different paltform
    (Mac platform TBD)

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Shell-Tools
 Different kinds of shell tools developed by Python are used for controlling PC and dealing small errands
+
+Files:
+shell_tools is the main program of this shell tool sets
+WolCmd.exe is the program which is used to wake on lan in Windows platform
+
+The following is an example for auto waking on LAN and remote connecting to the PC
+
+st = shell_tools()
+#set your remote PC MAC address here
+mac = 'xx-xx-xx-xx-xx-xx'
+mac = mac.lower()
+#set your local ip and netmask here
+st.cmd_wol(mac, '172.16.8.10', '255.255.255.0')
+#set your local scanning ip range 
+ip = st.cmd_rarp(mac, '172.16.8', 10, 20)
+if ip :
+    st.cmd_remote(ip)
+else:
+    print('Can not find the IP according to this mac: %s ' % mac)
---
+++ shell_tools.py
@@ -0,0 +1,126 @@
+from os import system
+from os import popen
+import re
+import threading
+import time
+import platform
+import socket
+
+class shell_tools:
+
+    def __init__(self):
+        cmd_dic = {
+            'Windows':{'ARP':'arp -a',
+                       'PING':'ping %s -n 1',
+                       'REMOTE': 'mstsc /v:%s',
+                       'WOL':'wolcmd %s %s %s 7'},
+            'Linux':{'ARP':'arp -a',
+                     'PING':'ping %s -c 1',
+                     'REMOTE':'vncviewer %s'},
+            'Mac':None #TBD
+            }
+        self.system = platform.system()
+        #print(self.system)
+        self.cmd_dic = cmd_dic.get(self.system)
+        #print(self.cmd_dic)
+        if not self.cmd_dic :
+            raise RuntimeError("None cmd list matched, please check the cmd_dic")
+
+    def cmd_ping(self, ip):
+        popen(self.cmd_dic['PING'] % ip)
+
+    def cmd_remote(self, ip):
+        popen(self.cmd_dic['REMOTE'] % ip)
+
+    def cmd_wol(self, mac, ip, mask):
+        #first convert the mac format to the command pattern
+        mac = mac.replace('-','')
+        ret = popen(self.cmd_dic['WOL'] % (mac, ip, mask)).read()
+        print(ret)
+
+    def cmd_rarp(self, mac, domain, start = 1, end = 255, timeout = 30):
+
+        start = max(1,start)
+        end = min(255,end)
+                
+        for i in range(0, timeout):
+            self.scan_lan(domain, start, end)
+            ret = self.find_ip(mac)
+            if ret :
+                return ret
+            time.sleep(1)
+        return None
+
+    def scan_lan (self, domain, start = 1, end = 255) :
+
+        start = max(1,start)
+        end = min(255,end)
+        
+        thread_list = []
+        for i in range (start, end):
+            t = threading.Thread(target = self.cmd_ping, args = (('%s.%d' % (domain, i)),))
+            thread_list.append(t)
+            t.start()
+            print("Thread %d is started \n" % i)
+
+        for t in thread_list:
+            t.join()
+
+    def find_ip (self, mac):
+        reg = r'((\d{1,3}\.){3}\d{1,3})\s*%s' % mac
+        ipre = re.compile(reg)
+        ret = popen(self.cmd_dic['ARP']).read()
+        iplist = re.findall(ipre,ret)
+
+        if iplist:
+            return iplist[0][0]
+        else:
+            return None
+
+    def check_idle_ip(self, domain, start = 1, end = 255) :
+        #scan the lan first to make sure the ARP cache is flushed
+        self.scan_lan(domain, start, end)
+        
+        reg = r'%s\.(\d{1,3})' % domain
+        ipre = re.compile(reg)
+        ret = popen(self.cmd_dic['ARP']).read()
+        iplist = re.findall(ipre,ret)
+        lastip = 0
+        idle_list = []
+
+        for ip in iplist:
+            iip = int(ip)
+            if iip >= start:
+                if not lastip:
+                    lastip = iip
+                else:
+                    for i in range(lastip + 1, iip):
+                        idle_list.append('%s.%d' % (domain,i))
+                lastip = iip
+
+        for i in range(lastip + 1, end):
+            idle_list.append('%s.%d' % (domain,i))
+
+
+        print('Idle IP is following')
+        for idle_ip in  idle_list :
+            print(idle_ip)
+        
+if __name__ == '__main__':  
+    # an example for auto wake on LAN and remote connect to the PC
+    
+    st = shell_tools()
+    #set your remote PC MAC address here
+    mac = 'xx-xx-xx-xx-xx-xx'
+    mac = mac.lower()
+    #set your local ip and netmask here
+    st.cmd_wol(mac, '172.16.8.10', '255.255.255.0')
+    #set your local scanning ip range 
+    ip = st.cmd_rarp(mac, '172.16.8', 10, 20)
+    if ip :
+        st.cmd_remote(ip)
+    else:
+        print('Can not find the IP according to this mac: %s ' % mac)
+    
+                
+        
